4eadddda07ab6e32692f0161ec8636004bf3b8c6
Rafael Horochovec <rl.horochovec@gmail.com>
2023-11-03 16:56:51 -0300

    Adicionando report

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'custom-title',
+    reportPageTitle: 'cypress-web-test',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
